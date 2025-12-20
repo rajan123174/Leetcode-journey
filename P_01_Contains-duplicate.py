88835d04556d9128b8a07e2ef7c2806fa249ba4a
Rajan <kaushalrajan914@gmail.com>
2025-12-20 11:51:21 +0530

    I forgot to call the method.

--- P_01_Contains-duplicate.py
+++ P_01_Contains-duplicate.py
@@ -1,3 +1,7 @@
 class Solution(object):
     def containsDuplicate(self, nums):
         return len(nums)!= len(set(nums))
+
+nums = [1,2,3,1]
+duplicate = Solution()
+print(duplicate.containsDuplicate(nums))
